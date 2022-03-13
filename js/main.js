e2e5cb367c6d358946767dfb052898c03c874191
jsurajkumar <62197637+jsurajkumar@users.noreply.github.com>
2022-03-13 22:08:55 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    location.origin.endsWith("sync-tube.de") && (window.ga = window.ga || function() {
+        (ga.q = ga.q || []).push(arguments)
+    }, ga.l = +new Date, ga("create", "UA-132357868-1", "auto"), ga("send", "pageview"))
+});
